aee4d440e7a115bfeec4c0a8a1c55bed8350f827
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-01-25 16:23:28 -0500

    so close

--- W3D3.rb
+++ W3D3.rb
@@ -1,50 +1,50 @@
-def range(start_num,end_num) #iterative range
-    return [] if end_num < start_num 
-    arr = []
-    (start_num...end_num).each do |i|
-        arr << i
-    end
+# def range(start_num,end_num) #iterative range
+#     return [] if end_num < start_num 
+#     arr = []
+#     (start_num...end_num).each do |i|
+#         arr << i
+#     end
 
-    arr
-end
+#     arr
+# end
 
-#recursive range
-def rec_range(start_num,end_num) #1,5
-    return [] if end_num < start_num
-    return [] if start_num == end_num 
-    # last_num = end_num -1
-    rec_range(start_num, end_num - 1) + [end_num - 1]
-end
+# #recursive range
+# def rec_range(start_num,end_num) #1,5
+#     return [] if end_num < start_num
+#     return [] if start_num == end_num 
+#     # last_num = end_num -1
+#     rec_range(start_num, end_num - 1) + [end_num - 1]
+# end
 
-# p rec_range(1,5)
+# # p rec_range(1,5)
 
 
-def range_sum(start_num,end_num) #iterative
-    arr = range(start_num,end_num)
-    sum = 0
-    arr.each do |num|
-        sum += num
-    end
-    sum
-end
+# def range_sum(start_num,end_num) #iterative
+#     arr = range(start_num,end_num)
+#     sum = 0
+#     arr.each do |num|
+#         sum += num
+#     end
+#     sum
+# end
 
-#recursive_sum
-def rec_range_sum(start_num, end_num)
-    #range 1,5
-    #1 +2 + 3 + 4
-    range = rec_range(start_num, end_num)
-    return 0 if range.length == 0 
-    last_num = range.pop
-    rec_range_sum(start_num, last_num) + last_num    
+# #recursive_sum
+# def rec_range_sum(start_num, end_num)
+#     #range 1,5
+#     #1 +2 + 3 + 4
+#     range = rec_range(start_num, end_num)
+#     return 0 if range.length == 0 
+#     last_num = range.pop
+#     rec_range_sum(start_num, last_num) + last_num    
 
-end
+# end
 
-# recursion 1 of exponentiation
-def exponentiation(base, exp) #2,1
-    return 1 if exp == 0
-    return base if exp == 1
-    base * exponentiation(base, exp - 1) #2 * exp(2, 1)
-end
+# # recursion 1 of exponentiation
+# def exponentiation(base, exp) #2,1
+#     return 1 if exp == 0
+#     return base if exp == 1
+#     base * exponentiation(base, exp - 1) #2 * exp(2, 1)
+# end
 
 # recursion 2 of exponentiation
 # def exponentiation(base, exp)
@@ -58,5 +58,42 @@ end
 
 # end
 
-p exponentiation(0,1)
+# p exponentiation(0,1)
 
+##Iterative Version
+# copy = arr.map! do |ele|
+#     if ele.is_a?(Array)
+#         ele.dup
+#     else
+#         ele
+#     end
+# end
+
+#base case self[0] =
+#checking it's an array
+#checking 
+# && array == array.flatten
+class Array
+    
+    def deep_dup(element)
+       return [element] if !element.is_a?(Array)
+
+       dupe = []
+
+      element.each do |ele|
+            if ele.is_a?(Array)
+                dupe + [ele]
+            else
+               deep_dup(ele)
+            end
+        end
+
+        dupe
+    end
+
+    puts deep_dup([["jamie"], [1,2,3]])
+     
+
+
+    end
+end
